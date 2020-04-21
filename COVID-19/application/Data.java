a47aca94b2d169302dfb15d90999703f36334586
Jack Sabinash <jsabinash@localhost>
2020-04-21 16:29:26 -0500

    added csv's for testing and Data type starter filess

---
+++ COVID-19/application/Data.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.util.ArrayList;
+
+public class Data {
+  ArrayList<String> dates;
+  ArrayList<ArrayList<Integer>> countries;
+  
+  Data() {
+    dates = new ArrayList<String>();
+    countries = new ArrayList<ArrayList<Integer>>();
+  }
+  
+  private void parseData() {
+    
+  }
+  
+}
